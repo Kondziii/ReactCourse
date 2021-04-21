f04a2fbac121cd2ef277317aea1d8aea73fbd914
Kondziii <1999konrad@gmail.com>
2021-04-21 21:38:00 +0200

    Filter feauture added

--- src/components/expenses/ExpenseList.js
+++ src/components/expenses/ExpenseList.js
@@ -1,10 +1,23 @@
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 import './ExpenseList.css';
+import '../newExpense/ExpenseFilter';
+import ExpensesFilter from '../newExpense/ExpenseFilter';
+import { useState } from 'react';
 
 const ExpenseList = (props) => {
+  const [filterYear, setFilterYear] = useState('2021');
+
+  const filterChangeHandler = (year) => {
+    setFilterYear(year);
+  };
+
   return (
     <Card className='expenses'>
+      <ExpensesFilter
+        selected={filterYear}
+        onChangeFilter={filterChangeHandler}
+      />
       {props.expenses.map((e) => {
         return (
           <ExpenseItem
